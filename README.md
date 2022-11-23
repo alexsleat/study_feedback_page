0169fa806b74f683b3db7aa176136b7287408667
Alex Sleat <alexsleat@gmail.com>
2022-11-23 13:11:15 +0100

    working version of study feedback form

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flask server with forms for feedback
+
+## Requirements:
+pip3 install flask gunicorn
+
+## Run:
+python3 app.py
---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, redirect, url_for, jsonify, request
+
+app = Flask(__name__)
+
+# list of questions.
+# ## Each questions is a list: 0: UID, 1: title, 2: response options
+QUESTIONS = [
+    ["Q01", "how do you feel about the robot", ["hate", "dislike","neutral", "like", "love"]],
+    ["Q02", "how was robots path", ["too cautious", "a bit cautious","neutral", "a bit aggressive", "too aggresive"]]
+]
+
+######################
+# Routes
+######################
+
+## Home ####################
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name = user))
+
+    return render_template('start.html')
+   
+
+## Questions Page ####################
+@app.route('/questions', methods=['POST', 'GET'])
+def showQuestions():
+    data = {}
+
+    # Check if data is coming back from the form:
+    if request.method == 'POST':
+        print("Responses: ")
+        response = {}
+        for i in range(len(QUESTIONS)):
+            current_q = QUESTIONS[i][0]
+            content = request.form[current_q]
+
+            ### Responses from the webpage are handled here:
+            print(current_q, content)
+
+        try:
+            return jsonify({'page': 'questions', 'success': True, 'responses' : data})
+        except Exception as exc:
+            print("Error executing SQL: %s"%exc)
+            return jsonify({'page': 'list', 'success': False})
+            
+    # Otherwise we show the questions:
+    return render_template('index.html', questions=QUESTIONS)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
