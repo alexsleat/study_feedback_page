425fc88a3be5752d22b98c4111f90a3bb40d7164
Alex Sleat <alexsleat@gmail.com>
2022-12-07 11:09:14 +0100

    small fixes

--- app.py
+++ app.py
@@ -10,6 +10,31 @@ QUESTIONS = [
     ["Q03", "radio_img", "Emoji Response", ["1", "2","3", "4", "5"]]
 ]
 
+USERID = -1
+TRIALID = -1
+
+### CSV Filename:
+CSV_FILENAME = 'survey_results.csv'
+
+def write_headers():
+
+    ### Check for header line:
+    first_line = ""
+    with open(CSV_FILENAME) as f:
+        first_line = f.readline().strip('\n')
+
+    ### Write file headers
+    h = "ParticipantID,TrialID,"
+    for i in range(len(QUESTIONS)):
+        current_q = QUESTIONS[i][0]
+        h = h + current_q + " Response" + ("," if i<len(QUESTIONS)-1 else "")
+
+    if(h == first_line):
+        print("Header exists")
+    else:
+        with open(CSV_FILENAME, 'a') as f:
+            f.write(h + "\n")
+
 ######################
 # Routes
 ######################
@@ -17,45 +42,67 @@ QUESTIONS = [
 ## Home ####################
 @app.route('/', methods=['POST', 'GET'])
 def home():
+
+    global USERID
+    global TRIALID
+
     if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name = user))
+        USERID = int(request.form['paricipantID'])
+        TRIALID = int(request.form['trialID'])
+        
+        print(USERID, TRIALID)
+
+        return render_template('bell.html', user=USERID, trial=TRIALID)
 
     return render_template('start.html')
 
 ## Bell ####################
 @app.route('/bell')
 def bell():
-    return render_template('bell.html')
+
+    global USERID
+    global TRIALID
+
+    return render_template('bell.html', user=USERID, trial=TRIALID)
 
 ## Questions Page ####################
 @app.route('/questions', methods=['POST', 'GET'])
 def showQuestions():
-    data = {}
 
+    global USERID
+    global TRIALID
+    
     # Check if data is coming back from the form:
     if request.method == 'POST':
         print("Responses: ")
-        response = {}
+        response_csv = ""
         for i in range(len(QUESTIONS)):
             current_q = QUESTIONS[i][0]
             content = request.form[current_q]
 
+            response_csv = response_csv + "," + content 
             ### Responses from the webpage are handled here:
-            print(current_q, content)
+            print(USERID, TRIALID, response_csv)
 
         try:
-            #return jsonify({'page': 'questions', 'success': True, 'responses' : data})
             #return render_template('submittedandredirect.html')
-            return render_template('bell.html')
+
+            ### Save results to file:
+            s = str(USERID) + "," + str(TRIALID) + response_csv 
+            with open(CSV_FILENAME, 'a') as f:
+                f.write(s + "\n")
+
+            TRIALID = TRIALID + 1
+            return render_template('bell.html', user=USERID, trial=TRIALID)
         except Exception as exc:
             print("Error executing SQL: %s"%exc)
             return jsonify({'page': 'list', 'success': False})
             
     # Otherwise we show the questions:
-    return render_template('index.html', questions=QUESTIONS)
+    return render_template('index.html', user=USERID, trial=TRIALID, questions=QUESTIONS)
 
 
 
 if __name__ == "__main__":
+    write_headers()
     app.run(debug=True, host="0.0.0.0")
