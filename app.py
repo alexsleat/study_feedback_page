8a5845ce16ff0a72ecca4df8f6ce18573b3af7c0
Alex Sleat <alexsleat@gmail.com>
2022-12-09 10:56:15 +0100

    first participant data

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ QUESTIONS = [
     ["Q02", "radio_text", "Did the robot move as you would have expected?", ["1", "2","3", "4", "5", "6", "7"]],
     #["Q03", "radio_text", "Did the robot move coherently with its task?", ["1", "2","3", "4", "5", "6", "7"]]
     #["Q03", "radio_img", "Emoji Response", ["1", "2","3", "4", "5"]]
-    ["Q03", "radio_img", "What was the robots task", ["artichoke", "basket","sandtimer"]]
+    ["Q03", "radio_img", "What was the robot's task", ["artichoke", "basket","sandtimer"]]
 ]
 
 USERID = -1
