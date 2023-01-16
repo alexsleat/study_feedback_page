e5deecdae26ae4afae31de2347f7b8085967d2d0
Alex Sleat <alexsleat@gmail.com>
2023-01-16 11:39:01 +0100

    created branch for rebecca vr project

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, jsonify, request
 from datetime import datetime
+from questions import QUESTIONS
 
 app = Flask(__name__)
-
-# list of questions.
-# ## Each questions is a list: 0: UID, 1: title, 2: response options
-QUESTIONS = [
-    ["Q01", "radio_text", "Did the robot move in an acceptable way?", ["1", "2","3", "4", "5", "6", "7"]],
-    ["Q02", "radio_text", "Did the robot move as you would have expected?", ["1", "2","3", "4", "5", "6", "7"]],
-    #["Q03", "radio_text", "Did the robot move coherently with its task?", ["1", "2","3", "4", "5", "6", "7"]]
-    #["Q03", "radio_img", "Emoji Response", ["1", "2","3", "4", "5"]]
-    ["Q03", "radio_img", "What was the robot's task", ["artichoke", "basket","sandtimer"]]
-]
 
 USERID = -1
 TRIALID = -1
---
+++ questions.py
@@ -0,0 +1,9 @@
+# list of questions.
+# ## Each questions is a list: 0: UID, 1: title, 2: response options
+QUESTIONS = [
+    ["Q01", "radio_text", "Did the robot move in an acceptable way?", ["1", "2","3", "4", "5", "6", "7"]],
+    ["Q02", "radio_text", "Did the robot move as you would have expected?", ["1", "2","3", "4", "5", "6", "7"]],
+    #["Q03", "radio_text", "Did the robot move coherently with its task?", ["1", "2","3", "4", "5", "6", "7"]]
+    #["Q03", "radio_img", "Emoji Response", ["1", "2","3", "4", "5"]]
+    ["Q03", "radio_img", "What was the robot's task", ["artichoke", "basket","sandtimer"]]
+]
