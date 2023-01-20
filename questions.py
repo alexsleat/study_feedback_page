7342d712443262341e3e2881f45395748c922195
becbot <62186734+becbot@users.noreply.github.com>
2023-01-20 11:29:19 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -14,6 +14,7 @@
 #     ]
     
 QUESTIONS = [
+    #Perceived Usability
         ["PU1_VR", "radio_text", "Using the virtual reality interface enhances my correction of robot errors.", ["1", "2", "3", "4", "5"] ],
         ["PU2_VR", "radio_text", "Using the virtual reality interface enhances my productivity.",               ["1", "2", "3", "4", "5"] ],
         ["PU3_VR", "radio_text", "Using the virtual reality interface enhances my effectiveness.",              ["1", "2", "3", "4", "5"] ],
@@ -24,22 +25,27 @@ QUESTIONS = [
         ["PU3_S", "radio_text", "Using the screen and keyboard interface enhances my effectiveness.",                   ["1", "2", "3", "4", "5"]],
         ["PU4_S", "radio_text", "I find the screen and keyboard interface useful.",                                     ["1", "2", "3", "4", "5"]],
 
+    #Perceived Ease of Use
         ["PEOU1_VR", "radio_text", "Learning to use the virtual reality interface is easy for me.",                     ["1", "2", "3", "4", "5"]],
         ["PEOU2_VR", "radio_text", "My interaction with the virtual reality interface is clear and understandable.",    ["1", "2", "3", "4", "5"]],
         ["PEOU3_VR", "radio_text", "It would be easy for me to become skillful at using the virtual reality interface.", ["1", "2", "3", "4", "5"]],
         ["PEOU4_VR", "radio_text", "I find the virtual reality interface easy to use.",                                 ["1", "2", "3", "4", "5"]],
+        ["PEOU5_VR", "radio_text", "It is easy to match the virtual world with the real world using the virtual reality interface.",                                 ["1", "2", "3", "4", "5"]],
 
         ["PEOU1_S", "radio_text", "Learning to use the screen and keyboard interface is easy for me.",                          ["1", "2", "3", "4", "5"]],
         ["PEOU2_S", "radio_text", "My interaction with the screen and keyboard interface is clear and understandable.",         ["1", "2", "3", "4", "5"]],
         ["PEOU3_S", "radio_text", "It would be easy for me to become skillful at using the screen and keyboard interface.",     ["1", "2", "3", "4", "5"]],
         ["PEOU4_S", "radio_text", "I find the screen and keyboard interface easy to use.",                                      ["1", "2", "3", "4", "5"]],
+        ["PEOU5_S", "radio_text", "It is easy to match the virtual world with the real world using the screen interface.",                                 ["1", "2", "3", "4", "5"]],
 
+    #Intention to Use
         ["IU1_VR", "radio_text", "Assuming I have access to the virtual reality interface for correcting robot errors, I intend to use it.",                    ["1", "2", "3", "4", "5"]],
         ["IU2_VR", "radio_text", "Given that I have access to the virtual reality interface for correcting robot errors, I predict that I would use it.",       ["1", "2", "3", "4", "5"]],
 
         ["IU1_S", "radio_text", "Assuming I have access to the screen and keyboard interface for correcting robot errors, I intend to use it.",                 ["1", "2", "3", "4", "5"]],
         ["IU2_S", "radio_text", "Given that I have access to the screen and keyboard interface for correcting robot errors, I predict that I would use it.",    ["1", "2", "3", "4", "5"]],
 
+    #Enjoyment
         ["E1_VR", "radio_text", "Using the virtual reality interface is fun.",          ["1", "2", "3", "4", "5"]],
         ["E2_VR", "radio_text", "Using the virtual reality interface is exciting.",     ["1", "2", "3", "4", "5"]],
         ["E3_VR", "radio_text", "Using the virtual reality interface is enjoyable.",    ["1", "2", "3", "4", "5"]],
@@ -48,6 +54,7 @@ QUESTIONS = [
         ["E2_S", "radio_text", "Using the screen and keyboard interface is exciting.",  ["1", "2", "3", "4", "5"]],
         ["E3_S", "radio_text", "Using the screen and keyboard interface is enjoyable.", ["1", "2", "3", "4", "5"]],
 
+    #Trust
         ["T1_VR", "radio_text", "The virtual reality interface is reliable.",           ["1", "2", "3", "4", "5"]],
         ["T2_VR", "radio_text", "The virtual reality interface is predictable.",        ["1", "2", "3", "4", "5"]],
         ["T3_VR", "radio_text", "The virtual reality interface is dependable.",         ["1", "2", "3", "4", "5"]],
@@ -56,7 +63,34 @@ QUESTIONS = [
         ["T1_S", "radio_text", "The screen and keyboard interface is reliable.",        ["1", "2", "3", "4", "5"]],
         ["T2_S", "radio_text", "The screen and keyboard interface is predictable.",     ["1", "2", "3", "4", "5"]],
         ["T3_S", "radio_text", "The screen and keyboard interface is dependable.",      ["1", "2", "3", "4", "5"]],
-        ["T4_S", "radio_text", "The screen and keyboard interface is consistent.",      ["1", "2", "3", "4", "5"]]
+        ["T4_S", "radio_text", "The screen and keyboard interface is consistent.",      ["1", "2", "3", "4", "5"]],
+
+    #Presence
+        ["AA1_VR", "radio_text", "How much were you able to control the events?",           ["1", "2", "3", "4", "5"]],
+        ["AA_VR", "radio_text", "How responsive was the environment to actions that you initiated (or performed)?",        ["1", "2", "3", "4", "5"]],
+        ["AA_VR", "radio_text", "Were you able to anticipate what would happen next in response to the actions that you performed?",         ["1", "2", "3", "4", "5"]],
+        ["AA_VR", "radio_text", "How completely were you able to actively survey or search the environment using vision?",         ["1", "2", "3", "4", "5"]],
+    
+        ["AA1_S", "radio_text", "How much were you able to control the events?",           ["1", "2", "3", "4", "5"]],
+        ["AA_S", "radio_text", "How responsive was the environment to actions that you initiated (or performed)?",        ["1", "2", "3", "4", "5"]],
+        ["AA_S", "radio_text", "Were you able to anticipate what would happen next in response to the actions that you performed?",         ["1", "2", "3", "4", "5"]],
+        ["AA_S", "radio_text", "How completely were you able to actively survey or search the environment using vision?",         ["1", "2", "3", "4", "5"]],
+    
+        ["AE1_VR", "radio_text", "How closely were you able to examine objects?",        ["1", "2", "3", "4", "5"]],
+        ["AE2_VR", "radio_text", "How well could you examine objects from multiple viewpoints?",     ["1", "2", "3", "4", "5"]],
+        ["AE3_VR", "radio_text", "How well could you concentrate on the assigned tasks or required activities rather than on the mechanisms used to perform those tasks or activities?",      ["1", "2", "3", "4", "5"]],
+    
+        ["AE1_S", "radio_text", "How closely were you able to examine objects?",        ["1", "2", "3", "4", "5"]],
+        ["AE2_S", "radio_text", "How well could you examine objects from multiple viewpoints?",     ["1", "2", "3", "4", "5"]],
+        ["AE3_S", "radio_text", "How well could you concentrate on the assigned tasks or required activities rather than on the mechanisms used to perform those tasks or activities?",      ["1", "2", "3", "4", "5"]],
+    
+        ["IQ1_VR", "radio_text", "How much delay did you experience between your actions and expected outcomes?",        ["1", "2", "3", "4", "5"]],
+        ["IQ2_VR", "radio_text", "How much did the visual display quality interfere or distract you from performing assigned tasks or required activities?",     ["1", "2", "3", "4", "5"]],
+        ["IQ3_VR", "radio_text", "How much did the control devices interfere with the performance of assigned tasks or with other activities?",      ["1", "2", "3", "4", "5"]],
+    
+        ["IQ1_S", "radio_text", "How much delay did you experience between your actions and expected outcomes?",        ["1", "2", "3", "4", "5"]],
+        ["IQ2_S", "radio_text", "How much did the visual display quality interfere or distract you from performing assigned tasks or required activities?",     ["1", "2", "3", "4", "5"]],
+        ["IQ3_S", "radio_text", "How much did the control devices interfere with the performance of assigned tasks or with other activities?",      ["1", "2", "3", "4", "5"]]
 
 ]
 
