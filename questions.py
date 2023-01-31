078cfd7b6d1f11f40ac767f5ada446e685ca3b08
becbot <62186734+becbot@users.noreply.github.com>
2023-01-31 13:12:45 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -52,6 +52,13 @@ QUESTIONS = [
         ["IQ1_S", "radio_text", "I experienced delays between my actions and expected outcomes.",                                                ["1", "2", "3", "4", "5"], ["Strongly Disagree", "Strongly Agree"] ],
         ["IQ2_S", "radio_text", "The visual display quality interfered or distracted me from performing assigned tasks or required activities.",     ["1", "2", "3", "4", "5"], ["Strongly Disagree", "Strongly Agree"] ],
         ["IQ3_S", "radio_text", "The control devices interfered with the performance of assigned tasks or with other activities.",                  ["1", "2", "3", "4", "5"], ["Strongly Disagree", "Strongly Agree"] ],
+    
+        ["P1_VR", "radio_text", "I adjusted quickly to the virtual environment experience.",                                                ["1", "2", "3", "4", "5"], ["Strongly Disagree", "Strongly Agree"] ],
+        ["P2_VR", "radio_text", "I felt proficient in moving and interacting with the virtual environment at the end of the experience",     ["1", "2", "3", "4", "5"], ["Strongly Disagree", "Strongly Agree"] ],
+        
+        ["P1_S", "radio_text", "I adjusted quickly to the screen and keyboard experience",                                                ["1", "2", "3", "4", "5"], ["Strongly Disagree", "Strongly Agree"] ],
+        ["P2_S", "radio_text", "I felt proficient in moving and interacting with the screen environment at the end of the experience.",     ["1", "2", "3", "4", "5"], ["Strongly Disagree", "Strongly Agree"] ],
+    
 ]
 
 QUESTION_ORDER = {
@@ -68,6 +75,9 @@ QUESTION_ORDER = {
         "AE3_": 10,
         "IQ1_": 11,
         "IQ2_": 12,
-        "IQ3_": 13
+        "IQ3_": 13,
+        "P1_": 14
+        "P2_": 15
+    
 
 }
